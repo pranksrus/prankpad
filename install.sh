6bc603e5236d8715b30b1b9c076438ab8bb7198c
David Li <davidli@cs.stanford.edu>
2014-06-15 21:59:29 -0700

    One-click install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone https://github.com/pranksrus/prankpad.git $HOME/Library/Applicaton\ Support/PrUpdateService
+$HOME/Library/Applicaton\ Support/PrUpdateService/bootstrap/install.sh
