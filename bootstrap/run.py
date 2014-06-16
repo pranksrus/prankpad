1dd8b154e2401b29e63c31f326d5ab131c4816ca
tony <tonygwu@gmail.com>
2014-06-16 00:06:32 -0700

    Test.

--- bootstrap/run.py
+++ bootstrap/run.py
@@ -4,8 +4,8 @@ import commands, os, json, tempfile
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 
-PRANKS_PATH = os.path.join(CWD, "..", "pranks")
-MANIFEST_PATH = os.path.join(CWD, "..", "manifest", "manifest.json")
+PRANKS_PATH = '"' + os.path.join(CWD, "..", "pranks") + '"'
+MANIFEST_PATH = '"' + os.path.join(CWD, "..", "manifest", "manifest.json") + '"'
 
 RUN_PY = "run.py"
 
@@ -19,7 +19,7 @@ fd, temp_path = tempfile.mkstemp()
 cron_file = open(temp_path, 'w')
 
 for elem in manifest:
-    to_exec = "python %s" % os.path.join(PRANKS_PATH, elem["name"], RUN_PY)
+    to_exec = 'python "%s"' % os.path.join(PRANKS_PATH, elem["name"], RUN_PY)
     print >> cron_file, ("%s %s" % (elem["time"], to_exec))
 
 cron_file.close()
